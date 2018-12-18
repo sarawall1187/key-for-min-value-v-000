3d834ca0b56a6c2e037388f5b8f8a4d6b4d640b7
Sara Walker <sarawall1187@gmail.com>
2018-12-18 17:49:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,10 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
       lowest_value = value 
-    
+      lowest_key = key 
     end
   end
+  lowest_key 
 end
 
 #return name of lowest value compared to other values 
